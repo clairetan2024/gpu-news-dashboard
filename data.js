44e14e1c56ccf0bc17084d9fd11563a493ebe462
clairetan2024 <clairetan2024@users.noreply.github.com>
2026-02-17 20:26:14 +0000

    deploy: 247f22d451fe05c352971bf7ca5e70a191d93fe0

--- data.js
+++ data.js
@@ -1,5 +1,5 @@
 const gpuData = {
-  "updateTime": "2026-02-16 20:19",
+  "updateTime": "2026-02-17 20:26",
   "enterprise": [
     {
       "name": "字节跳动 (ByteDance)",
@@ -79,7 +79,7 @@ const gpuData = {
   "news": [
     {
       "source": "System",
-      "date": "20:19",
+      "date": "20:26",
       "title": "当前网络波动，暂未获取到最新源，请稍后重试。",
       "url": "#"
     },
